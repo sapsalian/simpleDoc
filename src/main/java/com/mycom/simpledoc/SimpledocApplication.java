844f21d0251a729fa599c04d0ba0c4be087c52f3
Yongjin An <yongjin5352@gmail.com>
2024-08-13 20:44:59 +0900

    initiate project

---
+++ src/main/java/com/mycom/simpledoc/SimpledocApplication.java
@@ -0,0 +1,13 @@
+package com.mycom.simpledoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpledocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpledocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycom/simpledoc/SimpledocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycom.simpledoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpledocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
